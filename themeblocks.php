77ed39355e09da146b7571f59316e65760273b62
Jacob Thygesen <info@jaxweb.dk>
2025-05-27 21:17:45 +0200

    Update themeblocks.php

--- themeblocks.php
+++ themeblocks.php
@@ -1,1 +1,15 @@
-<php
+<?php
+/**
+ * Plugin Name: Dynamic Blocks Plugin
+ * Description: A plugin to create individual Gutenberg blocks as needed.
+ * Version: 1.0
+ * Author: WebJax
+ */
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Include block registration.
+require_once plugin_dir_path(__FILE__) . 'block-registration.php';
